f7cb9a1b6a36787ab0f71777a405b763e02163b1
edvardunsvag <Edvard.Unsvag@trumf.no>
2025-03-06 16:20:50 +0100

    comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import QuizGenerator from "@/components/QuizGenerator"
 
+//Comment to run main hook
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
