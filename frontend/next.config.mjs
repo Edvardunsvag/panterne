2308e93320226cf931489f4596054583d26f1414
Edvard Unsvåg <edvard.unsvag@EdvardUnsvag-sin-MacBook-Pro.local>
2025-08-18 14:03:25 +0200

    workflow changes, debug

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,8 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',  // Optimizes for deployment
-  experimental: {
-    outputFileTracingRoot: process.cwd(),
-  },
+  output: 'standalone',
   images: {
-    unoptimized: true,   // May be needed for Azure App Service
+    unoptimized: true,
   },
 };
 
